0431a9d6658df0c0a43283c1178937f1fc4a2f88
riclunar <63484908+riclunar@users.noreply.github.com>
2020-06-02 11:02:00 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/riclr/implementacionjson/Datos.java
@@ -0,0 +1,33 @@
+package mx.edu.tesoem.isc.riclr.implementacionjson;
+
+public class Datos {
+
+
+    String Nombre;
+    String Edad;
+    String Sexo;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(String edad) {
+        Edad = edad;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String sexo) {
+        Sexo = sexo;
+    }
+}
